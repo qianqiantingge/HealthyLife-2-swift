2f80f7c13343762c9c5fca3e1d600e4a548788be
qianqiantingge <qianfeng@kimch.local>
2016-09-21 15:14:22 +0800

    Initial Commit

---
+++ HealthyLife/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HealthyLife
+//
+//  Created by qianfeng on 16/9/21.
+//  Copyright © 2016年 kimCH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
